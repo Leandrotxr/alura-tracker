2ab642c9b3d29a9b0be69a7bf31203cca919c39a
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-22 15:35:26 -0300

    feat: adicionando novas funcionalidade

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,30 +10,11 @@
           type="text"
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>
-              {{tempoDecorrido}}
-            </strong>
-          </section>
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -41,28 +22,24 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
   name: "FormulárioTracker",
-  data() {
-      return {
-        tempoEmSegundos: 0,
-        cronometro: 0
-      }
+  components: {
+    Temporizador
   },
-  computed: {
-    tempoDecorrido () : string{
-        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+  data() {
+    return{
+        descricao: ''
     }
   },
-  methods:{
-    iniciar(){
-        this.cronometro = setInterval(() => {
-            this.tempoEmSegundos += 1
-        }, 1000)
-    },
-    finalizar(){
-        clearInterval(this.cronometro)
+  methods: {
+    finalizarTarefa(tempoDecorrido: number) : void{
+        console.log(tempoDecorrido)
+        console.log(this.descricao)
+
+        this.descricao = ''
     }
   }
 });
