60aa244b83bb856b368602072041e1aee6da4ac9
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-22 16:48:20 -0300

    feat: adicionando lista estática

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da tarefa
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="15"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Cronometro from './Cronometro.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TarefaF',
+    components: {
+        Cronometro
+    }
+})
+</script>
+
+<style scoped>
+.box{
+    background: #FAF0CA;
+}
+</style>
