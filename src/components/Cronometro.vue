c9179320938fc14fb9a930bfe2b928c08abd8280
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-23 09:20:12 -0300

    feat: adicionando lista dinâmica e modo escuro

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -1,26 +1,32 @@
 <template>
-    <section>
-        <strong>
-            {{ tempoDecorrido }}
-        </strong>
-    </section>
+  <section>
+    <strong class="display">
+      {{ tempoDecorrido }}
+    </strong>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'CronometroTempo',
-    props: {
-        tempoEmSegundos: {
-            type: Number,
-            default: 0
-        }
-    },
-    computed: {
-        tempoDecorrido () : string {
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-        }
-     },
+  name: 'CronometroTempo',
+  props: {
+    tempoEmSegundos: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    tempoDecorrido () : string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+    }
+  },
 })
 </script>
+
+<style scoped>
+.display {
+  color: var(--texto-primario);
+}
+</style>
