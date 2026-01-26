31bee2fb040d2a2785436926f8f5e73caf400bc6
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-26 15:31:53 -0300

    feat: finalizando as notificações

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,21 +1,19 @@
 <template>
-    <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-        <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-          <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+  <section class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </section>
 </template>
 
 <script lang="ts">
@@ -24,30 +22,35 @@ import Cronometro from "./Cronometro.vue";
 
 export default defineComponent({
   name: "Temporizador",
-  emits: ['aoTemporizadorFinalizado'],
+  emits: ['aoFinalizarTarefa'],
   components: {
-    Cronometro
+    Cronometro,
   },
-  data() {
-      return {
-        tempoEmSegundos: 0,
-        cronometro: 0,
-        cronometroRodando: false
-      }
+  data () {
+    return {
+      tempoEmSegundos: 0,
+      cronometroRodando: false,
+      cronometro: 0 
+    }
   },
-  methods:{
-    iniciar(){
-        this.cronometroRodando = true
-        this.cronometro = setInterval(() => {
-            this.tempoEmSegundos += 1
-        }, 1000)
+  methods: {
+    iniciar () : void {
+      this.cronometroRodando = true
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1
+      }, 1000)
     },
-    finalizar(){
-        this.cronometroRodando = false
-        clearInterval(this.cronometro)
-        this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
-        this.tempoEmSegundos = 0
+    finalizar () : void {
+      this.$emit('aoFinalizarTarefa', this.tempoEmSegundos)
+      this.tempoEmSegundos = 0
+      this.cronometroRodando = false
+      clearInterval(this.cronometro)
     }
   }
 });
 </script>
+<style scoped>
+.button {
+  margin-left: 8px;
+}
+</style>
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -22,8 +22,9 @@
 </template>
 
 <script lang="ts">
+import { TipoNotificacao } from '@/interfaces/INotificacao';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes';
+import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes';
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router'
 
@@ -57,6 +58,11 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '';
+            this.store.commit(NOTIFICAR, {
+                titulo: 'Novo projeto adicionado',
+                texto: 'Seu projeto já está disponível!',
+                tipo: TipoNotificacao.SUCESSO
+            })
             this.router.push('/projetos')
         }
     },
