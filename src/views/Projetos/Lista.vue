302a367f441237acf9e16fc0481d809df078493a
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-27 10:18:45 -0300

    feat: adicionando json-server

--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -37,6 +37,7 @@
 
 <script lang="ts">
 import { useStore } from '@/store';
+import { OBTER_PROJETOS } from '@/store/tipo-acoes';
 import { EXCLUIR_PROJETO } from '@/store/tipo-mutacoes';
 import { computed, defineComponent } from 'vue';
 
@@ -49,6 +50,7 @@ export default defineComponent({
     },
     setup () {
         const store = useStore()
+        store.dispatch(OBTER_PROJETOS)
         return {
             projetos: computed(() => store.state.projetos),
             store
