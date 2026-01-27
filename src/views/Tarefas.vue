02be4fa00bd574da3a437d0f5ea9b505b501c2ae
BRDC CCXCI(Leandro_BRSC) <bWX1488086@china.huawei.com>
2026-01-27 13:03:38 -0300

    feat: adicionando o modal para editar tarefas

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -6,14 +6,32 @@
         </Box>
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" style="color: black;"/>
     </div>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modal title</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+        <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Salvar</button>
+          <buttton class="button">Cancelar</buttton>
+        </footer>
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Formulario from '../components/Formulario.vue'
 import Tarefa from '../components/Tarefa.vue'
 import Box from '../components/Box.vue'
 import ITarefa from '../interfaces/ITarefa';
+import { useStore } from '@/store';
+import { CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/tipo-acoes';
 
 export default defineComponent({
   name: 'App',
@@ -22,19 +40,23 @@ export default defineComponent({
     Tarefa,
     Box
   },
-  data () {
-    return {
-      tarefas: [] as ITarefa[]
-    }
-  },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
+    salvarTarefa (tarefa: ITarefa) : void {
+      this.store.dispatch(CADASTRAR_TAREFA, tarefa) 
     }
   },
   computed: {
     semTarefa () : boolean {
       return this.tarefas.length === 0
+    }
+  },
+  setup () {
+    const store = useStore()
+    store.dispatch(OBTER_TAREFAS)
+    store.dispatch(OBTER_PROJETOS)
+    return {
+      tarefas: computed(() => store.state.tarefas),
+      store
     }
   }
 });
